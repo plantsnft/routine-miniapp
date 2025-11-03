6f2725c0c3068bdefbadfb4537bb887f371d23e9
plantsnft <cpjets07@yahoo.com>
2025-11-03 14:57:10 -0800

    Add fc:miniapp meta tag to help Warpcast recognize mini app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export async function generateMetadata(): Promise<Metadata> {
     },
     other: {
       "fc:frame": JSON.stringify(getMiniAppEmbedMetadata()),
+      "fc:miniapp": JSON.stringify(getMiniAppEmbedMetadata()),
     },
   };
 }

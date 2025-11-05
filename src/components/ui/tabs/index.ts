c78acd9cad0cd132d85f03cc3ab7b1c845e76b03
plantsnft <cpjets07@yahoo.com>
2025-11-04 21:56:57 -0800

    Fix FeedTab export in tabs index

--- src/components/ui/tabs/index.ts
+++ src/components/ui/tabs/index.ts
@@ -1,5 +1,6 @@
 export { HomeTab } from './HomeTab';
 export { LeaderboardTab } from './LeaderboardTab';
+export { FeedTab } from './FeedTab';
 export { ActionsTab } from './ActionsTab';
 export { ContextTab } from './ContextTab';
 export { WalletTab } from './WalletTab'; 
